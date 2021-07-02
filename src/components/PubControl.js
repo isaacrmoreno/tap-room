cf42d1d86aa40a09ec8092bbcb10eee26c9fcf0a
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-02 11:20:01 -0700

    :beers: add handle click method

--- src/components/PubControl.js
+++ src/components/PubControl.js
@@ -1,1 +1,32 @@
 import React from 'react';
+
+class PubControl extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      formVisibleOnPage: false,
+      masterKegList: [],
+      selectedKeg: null,
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedKeg != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  
+
+
+
+} 
