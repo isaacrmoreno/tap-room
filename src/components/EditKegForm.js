b943ac6082c866f95db4824c78ee5647c967f57e
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-02 14:13:48 -0700

    :memo: typo fix

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -7,8 +7,8 @@ function EditKegForm(props) {
 
   function handleEditKegFromSubmission(event) {
     event.preventDefault();
-    props.onEditKeg({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, id: keg.id
-    })
+    props.onEditKeg({ name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, id: keg.id});
+  }
 
     return (
       <React.Fragment>
@@ -18,7 +18,7 @@ function EditKegForm(props) {
       </React.Fragment>
     );
   }
-}
+
 
 EditKegForm.propTypes = {
   keg: PropTypes.object,
