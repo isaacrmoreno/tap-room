e6be7c8df66467ad5b13329fba09f0e4021bfd31
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-02 12:15:17 -0700

    :beers: import PubControl component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import PubControl from './PubControl'
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <PubControl/>
     </React.Fragment>
   );
 }
